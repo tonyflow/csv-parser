f17eec3df5f1c2f8f4f014869fcce31cd0409fae
Niko Strongioglou <nikolaos@veezoo.com>
2020-03-01 14:42:03 +0100

    Add configuration properties

--- src/main/scala/com/onedot/helper/Settings.scala
+++ src/main/scala/com/onedot/helper/Settings.scala
@@ -1,5 +1,20 @@
 package com.onedot.helper
 
+import com.onedot.helper.SettingConstants._
+import com.typesafe.config.ConfigFactory
+
 object Settings {
 
+  private val config = ConfigFactory.load("parser")
+  val quotingString = config.getString(QUOTING_STRING)
+  val lineSeparator = config.getString(LINE_SEPARATOR)
+  val fieldDelimiter = config.getConfig(FIELD_DELIMITER)
+
+}
+
+
+object SettingConstants {
+  final val QUOTING_STRING = "quoting-string"
+  final val LINE_SEPARATOR = "line-separator"
+  final val FIELD_DELIMITER = "field-delimiter"
 }
