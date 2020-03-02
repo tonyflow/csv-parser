330afa933d75b6d16e61044a4573ea3d994ab66c
Niko Strongioglou <nikolaos@veezoo.com>
2020-03-02 18:11:33 +0100

    Add new tests - still no assertions included

--- src/test/scala/com/onedot/CSVParserSpec.scala
+++ src/test/scala/com/onedot/CSVParserSpec.scala
@@ -8,12 +8,23 @@ import scala.concurrent.Await
 
 class CSVParserSpec extends AnyFlatSpec {
 
-  "CSV Parser" should "be able to read bytes from the file" in {
-    CSVParser.byteParsing("/test.csv")
+  "CSV Parser"  should "read lines from the file" in {
+    Await.result(CSVParser.parse("/test.csv"), 1.minute).foreach(println(_))
   }
 
-  it should "read lines from the file" in {
-    Await.result(CSVParser.parse("/test.csv"), 1.minute).foreach(println(_))
+  it should "be able to handle new line characters embedded in a quoted cell" in {
+    Await.result(CSVParser.parse("/new-line-in-quoted-cell.csv"), 1.minute).foreach(println(_))
+  }
+
+  it should "be able to process field delimiters in quoted cells" in {
+    Await.result(CSVParser.parse("/field-delimiters-in-quoted-cells.csv"), 1.minute).foreach(println(_))
+  }
+
+  /**
+   * The convention here is that an absent element should be treated like an empty string when parsed
+   */
+  it should "be able to process absent elements" in {
+    Await.result(CSVParser.parse("/absent-element-at-the-end-of-the-line.csv"), 1.minute).foreach(println(_))
   }
 
 }
