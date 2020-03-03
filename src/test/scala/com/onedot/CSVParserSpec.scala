5c3942756d0fb73e2d22b118ade08bb3dbd3da8b
Niko Strongioglou <nikolaos@veezoo.com>
2020-03-03 10:37:28 +0100

    Add assertions

--- src/test/scala/com/onedot/CSVParserSpec.scala
+++ src/test/scala/com/onedot/CSVParserSpec.scala
@@ -8,23 +8,27 @@ import scala.concurrent.Await
 
 class CSVParserSpec extends AnyFlatSpec {
 
-  "CSV Parser"  should "read lines from the file" in {
-    Await.result(CSVParser.parse("/test.csv"), 1.minute).foreach(println(_))
+  "CSV Parser" should "read lines from the file" in {
+    val result = Await.result(CSVParser.parse("/test.csv"), 1.minute)
+    assert(result.size == 100)
   }
 
   it should "be able to handle new line characters embedded in a quoted cell" in {
-    Await.result(CSVParser.parse("/new-line-in-quoted-cell.csv"), 1.minute).foreach(println(_))
+    val result = Await.result(CSVParser.parse("/new-line-in-quoted-cell.csv"), 1.minute)
+    assert(result(0) == Vector("a", "\"a split cell\"", "b", "\"something else\""))
   }
 
   it should "be able to process field delimiters in quoted cells" in {
-    Await.result(CSVParser.parse("/field-delimiters-in-quoted-cells.csv"), 1.minute).foreach(println(_))
+    val result = Await.result(CSVParser.parse("/field-delimiters-in-quoted-cells.csv"), 1.minute)
+    assert(result(0) == Vector("a", "\"b,c,d\"", "e"))
   }
 
   /**
    * The convention here is that an absent element should be treated like an empty string when parsed
    */
   it should "be able to process absent elements" in {
-    Await.result(CSVParser.parse("/absent-element-at-the-end-of-the-line.csv"), 1.minute).foreach(println(_))
+    val result = Await.result(CSVParser.parse("/absent-element-at-the-end-of-the-line.csv"), 1.minute)
+    assert(result(0) == Vector("", "a", "b", "", "", "", "c", ""))
   }
 
 }
